80675a9d15009854a2244c7c59f38e891f3750a4
Chris Amico <eyeseast@gmail.com>
2021-04-04 14:24:22 -0400

    initial

---
+++ README.md
@@ -0,0 +1,38 @@
+# dorchester
+
+[![PyPI](https://img.shields.io/pypi/v/dorchester.svg)](https://pypi.org/project/dorchester/)
+[![Changelog](https://img.shields.io/github/v/release/eyeseast/dorchester?include_prereleases&label=changelog)](https://github.com/eyeseast/dorchester/releases)
+[![Tests](https://github.com/eyeseast/dorchester/workflows/Test/badge.svg)](https://github.com/eyeseast/dorchester/actions?query=workflow%3ATest)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/eyeseast/dorchester/blob/master/LICENSE)
+
+A toolkit for making dot-density maps in Python
+
+## Installation
+
+Install this tool using `pip`:
+
+    $ pip install dorchester
+
+## Usage
+
+Usage instructions go here.
+
+## Development
+
+To contribute to this tool, first checkout the code. Then create a new virtual environment:
+
+    cd dorchester
+    python -mvenv venv
+    source venv/bin/activate
+
+Or if you are using `pipenv`:
+
+    pipenv shell
+
+Now install the dependencies and tests:
+
+    pip install -e '.[test]'
+
+To run the tests:
+
+    pytest
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1"
+
+
+requirements = ["click", "fiona", "geojson", "numpy", "shapely"]
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="dorchester",
+    description="A toolkit for making dot-density maps in Python",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Chris Amico",
+    url="https://github.com/eyeseast/dorchester",
+    project_urls={
+        "Issues": "https://github.com/eyeseast/dorchester/issues",
+        "CI": "https://github.com/eyeseast/dorchester/actions",
+        "Changelog": "https://github.com/eyeseast/dorchester/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["dorchester"],
+    entry_points="""
+        [console_scripts]
+        dorchester=dorchester.cli:cli
+    """,
+    install_requires=requirements,
+    extras_require={"test": ["pytest", "matplotlib"]},
+    tests_require=["dorchester[test]"],
+    python_requires=">=3.6",
+)
