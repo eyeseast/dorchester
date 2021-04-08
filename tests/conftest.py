7c22fff818aa76dab6d829a10f48250a936fa6a9
Chris Amico <eyeseast@gmail.com>
2021-04-07 20:38:02 -0400

    need that

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import geojson
+import pytest
+
+
+def feature(id=None, vertices=5, **properties):
+    "Generate a random feature"
+    geometry = geojson.utils.generate_random("Polygon", vertices)
+    return geojson.Feature(id, geometry, properties)
+
+
+@pytest.fixture()
+def feature_collection():
+    features = [feature(i, population=100, households=20) for i in range(10)]
+    return geojson.FeatureCollection(features)
+
+
+@pytest.fixture()
+def source(tmp_path, feature_collection):
+    path = tmp_path / "fc.geojson"
+    with path.open("w") as f:
+        geojson.dump(feature_collection, f)
+
+    return path
